2b7d996a5eaa419875cd65284d1ba7079ae99a1e
FooTage <pookair@gmail.com>
2020-01-14 00:32:47 +0200

    No minus, persistant, reciever has to exsist

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const database1 = require('./database');
+const express = require('express');
+const app = express();
+const port = 3001;
 
 database = {
     jo: {balance: 100}
@@ -12,44 +13,26 @@ app.use(express.json());
 app.get('/balance/:name', (req, res)=> {
     // Authentication?
     var userName = req.params.name;
-    var balance = database[userName];
+    var balance = database1.getBalance(userName);
     res.send(balance);})
 
 
 app.post('/send', (req, res)=> {
     // Authentication
     var sendRequest = req.body;
+    var sender = sendRequest.Sender;
+    var reciever = sendRequest.Reciever;
+    var amount = sendRequest.Amount;
     // test correct request
-    var success = updateDatabase(sendRequest);
-    if (success) {
-        res.sendStatus(200)
+    var returnCode = database1.transfer(sender, reciever, amount);
+    if (returnCode != 0) {
+        res.status(400).send(returnCode);
     }
-    else {
-        res.sendStatus(403)
-    }
-
+    
+    res.sendStatus(200);
     })
 
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-function updateDatabase(request) {
-    // Reciever Amount Sender
-    var reciever = request.Reciever;
-    var amount = request.Amount;
-    var sender = request.Sender;
-
-    if (database.hasOwnProperty(sender)) {
-        database[sender].balance -= amount;
-        if (!(database.hasOwnProperty(reciever))) {
-            database[reciever] = {balance : 0};
-        }
-        database[reciever].balance += amount;
-    }
-    else {
-        return false;
-    }
-    return true;
-
-}
